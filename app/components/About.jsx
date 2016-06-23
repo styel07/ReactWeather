c741b2a51ff88eb7f16e92a22dd07e6589f5ba08
Eleazar Corpuz <eleazarcorpuz@gmail.com>
2016-06-23 02:13:40 -0700

    style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,16 +2,27 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About</h3>
+    <div>
+      <h1 classNAme="text-center">About</h1>
+      <p>
+        This is a simple weather app that allows you to check current
+        temperature of a given city.
+      </p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> This was the JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> I used Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
+
+  </div>
+
   );
 }
 
 module.exports = About;
-
-// var About = React.createClass({
-//   render: function() {
-//     return (
-//       <h3>About Component</h3>
-//     );
-//   }
-// });
